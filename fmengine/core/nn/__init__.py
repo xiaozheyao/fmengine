e52e874c0814b946b0d8e3dbc4056858e0ab7439
Xiaozhe Yao <askxzyao@gmail.com>
2024-08-26 21:24:08 +0200

    disable liger_kernel

--- fmengine/core/nn/__init__.py
+++ fmengine/core/nn/__init__.py
@@ -3,12 +3,12 @@ from .embedding import Llama3ScaledRoPE, RotaryPositionalEmbeddings
 from .linear.mlp import FeedForward
 from .norm.rms import RMSNorm
 from .norm.fused_rms import FusedRMSNorm
-from .norm.liger_rms import LigerRMSNorm
+# from .norm.liger_rms import LigerRMSNorm
 from .optimizer.optimizer import build_optimizer
 from .optimizer.scheduler import build_lr_scheduler
 from .transformers import TiedEmbeddingTransformerDecoder, TransformerDecoder, TransformerDecoderLayer
 from .loss.cross_entropy import cross_entropy_loss
-from .loss.liger_ce import liger_cross_entropy_loss
+# from .loss.liger_ce import liger_cross_entropy_loss
 
 __all__ = [
     "FeedForward",
@@ -19,10 +19,10 @@ __all__ = [
     "RotaryPositionalEmbeddings",
     "Llama3ScaledRoPE",
     "RMSNorm",
-    "LigerRMSNorm",
+    # "LigerRMSNorm",
     "build_optimizer",
     "FusedRMSNorm",
     "build_lr_scheduler",
     "cross_entropy_loss",
-    "liger_cross_entropy_loss",
+    # "liger_cross_entropy_loss",
 ]
